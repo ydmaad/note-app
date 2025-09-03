34c16627c492da6e6048bf8529b68866e2de74e2
Minae <ydmaad@gmail.com>
2025-09-04 01:01:48 +0900

    feat: 더미 데이터 추가, NoteItem 생성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import type { Note } from "./types";
+import NoteItem from "./components/NoteItem";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: "1",
+      title: "장볼거",
+      content: "물, 과자, 라면",
+      noteColor: "pink",
+      priority: "high",
+      tags: ["todo"],
+      status: "normal",
+      createAt: new Date(),
+    },
+    {
+      id: "2",
+      title: "장볼거2",
+      content: "물2, 과자2, 라면2",
+      noteColor: "blue",
+      priority: "low",
+      tags: ["todo", "exercise"],
+      status: "normal",
+      createAt: new Date(),
+    },
+    {
+      id: "3",
+      title: "장볼거3",
+      content: "물3, 과자3, 라면3",
+      noteColor: "white",
+      priority: "high",
+      tags: ["todo", "study"],
+      status: "archived",
+      createAt: new Date(),
+    },
+    {
+      id: "4",
+      title: "장볼거4",
+      content: "물4, 과자4, 라면4",
+      noteColor: "white",
+      priority: "high",
+      tags: ["todo", "study"],
+      status: "normal",
+      createAt: new Date(),
+    },
+    {
+      id: "5",
+      title: "장볼거5",
+      content: "물5, 과자5, 라면5",
+      noteColor: "yellow",
+      priority: "high",
+      tags: ["todo", "study"],
+      status: "normal",
+      createAt: new Date(),
+    },
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {notes
+        .filter((note) => note.status === "normal")
+        .map((note) => (
+          <NoteItem key={note.id} note={note} />
+        ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/NoteItem.tsx
@@ -0,0 +1,88 @@
+import { BsPinAngle, BsPinFill } from "react-icons/bs";
+import type { Note } from "../types";
+import { IoIosCreate } from "react-icons/io";
+import { RiDeleteBinFill } from "react-icons/ri";
+import { HiArchive } from "react-icons/hi";
+
+interface NoteItemProps {
+  note: Note;
+}
+
+const NoteItem = ({ note }: NoteItemProps) => {
+  // 노트별 배경색
+  const getColorClass = (color: string) => {
+    switch (color) {
+      case "white":
+        return "bg-white";
+      case "blue":
+        return "bg-blue-100";
+      case "pink":
+        return "bg-pink-100";
+      case "yellow":
+        return "bg-yellow-100";
+      default:
+        return "bg-gray-200";
+    }
+  };
+
+  // 작성일
+  const createdDate = note.createAt;
+  // 날짜 - 한국식
+  const datePart = createdDate.toLocaleDateString("ko-KR");
+  // 시간 - AM/PM
+  const timePart = createdDate.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+  return (
+    <div
+      key={note.id}
+      className={`${getColorClass(
+        note.noteColor
+      )} p-4 rounded-md shadow-xl/20 w-72 h-60 flex flex-col m-5`}
+    >
+      {/* 제목 + 우측 핀 */}
+      <div className="flex justify-between">
+        <h1 className="text-lg items-center font-semibold mb-2">
+          {note.title}
+        </h1>
+        <span className="px-2 py-1 rounded-full cursor-pointer">
+          {note.priority === "high" ? (
+            <BsPinFill className="text-red-500" />
+          ) : (
+            <BsPinAngle className="text-gray-500" />
+          )}
+        </span>
+      </div>
+
+      {/* 내용 */}
+      <span className="text-sm mb-3 break-words">{note.content}</span>
+
+      {/* 태그 + 날짜 + 버튼 */}
+      <div className="flex flex-col gap-2 mt-auto">
+        <div className="flex gap-1 flex-wrap">
+          {note.tags.map((tag) => (
+            <span
+              key={tag}
+              className="px-1 py-1 text-xs rounded-sm bg-gray-100 text-gray-600 opacity-70"
+            >
+              {" "}
+              #{tag}
+            </span>
+          ))}
+        </div>
+        <div className="flex justify-between">
+          <p className="text-xs">{`${datePart}${" "}${timePart}`}</p>
+          <div className="flex justify-between gap-2 cursor-pointer">
+            <IoIosCreate />
+            <HiArchive />
+            <RiDeleteBinFill />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoteItem;
