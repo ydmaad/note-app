443d09a1e8490f396111db553460ed917133d9de
Minae <ydmaad@gmail.com>
2025-09-05 14:12:37 +0900

    chore: 기본 레이아웃 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,12 @@ import MainContent from "./components/MainContent";
 
 const App = () => {
   return (
-    <div className="h-screen flex flex-col">
-      <Header />
-      <div className="flex flex-1">
+    <div className="h-screen flex flex-row">
+      <div className="w-64 flex-shrink-0">
         <Sidebar />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <Header />
         <MainContent />
       </div>
     </div>
