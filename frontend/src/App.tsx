6a8ef1da4e04f6de44e59310b749dac3fd9f8c21
Minae <ydmaad@gmail.com>
2025-09-07 23:58:46 +0900

    feat: useEffect로 서버의 데이터 로딩, dispatch 타입 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import MainContent from "./components/MainContent";
-import { useSelector } from "react-redux";
-import type { RootState } from "./store";
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "./store";
+import { fetchNotesFromServer } from "./store/notesSlice";
+import { useEffect } from "react";
 
 const App = () => {
   const notes = useSelector((state: RootState) => state.notes.notes);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchNotesFromServer());
+  }, [dispatch]);
+
   return (
     <div className="h-screen flex flex-row">
       <div className="w-64 flex-shrink-0">
